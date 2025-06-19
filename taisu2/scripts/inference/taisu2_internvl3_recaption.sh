f88715edcacedd051393a8befa60ea8cabc1382e
miznchimaki <miznchimaki@gmail.com>
2025-06-19 16:49:19 +0800

    tracked pip install files

--- taisu2/scripts/inference/taisu2_internvl3_recaption.sh
+++ taisu2/scripts/inference/taisu2_internvl3_recaption.sh
@@ -19,7 +19,7 @@ cd $HOME/projects/Taisu2/taisu2/
 start_time_stamp=$(date +%Y-%m-%d-%H:%M:%S)
 echo "Begin Taisu2 image-alttext pairs recaption (inference) at ${start_time_stamp}"
 
-deepspeed --include localhost:0,1,2,3,4,5,6,7 --master_port ${MASTER_PORT} \
+deepspeed --include localhost:0,1,4,5,6,7 --master_port ${MASTER_PORT} \
           ./llava/inference/recaption.py --recaption-idx 1 \
                                          --conv-template-name internvl2_5 \
                                          --num-workers 8 \
@@ -38,9 +38,9 @@ deepspeed --include localhost:0,1,2,3,4,5,6,7 --master_port ${MASTER_PORT} \
                                          --min-subimg-num 1 \
                                          --max-subimg-num 12 \
                                          --use-thumbnail True \
-                                         --tars-folder image-alttext-total-1.00M-at-2025-04-16-21:10:39 \
+                                         --tars-folder image-alttext-total-3.00M-at-2025-04-11-22:44:31 \
                                          --tars-subfolder rename_and_rearchive \
-                                         --total-samples 787991 \
+                                         --total-samples 2221331 \
                                          --wds-shuffle-seed None \
                                          --model-name-or-path ${MODEL_NAME_OR_PATH} \
                                          --data-type bfloat16 \
